ce6a36f9fa5b8db9e75835583f1ddd76d6f77bbc
Andrey14121992 <andrey1477@ukr.net>
2022-11-29 10:40:24 +0200

    Виправив

---
+++ src/components/theme.js
@@ -0,0 +1,30 @@
+export const theme = {
+    colors: {
+      text: '#228b22',
+      background: 'linear-gradient(45deg, #588bb0, #00d4b6, #fcf837)',
+      primary: '#454b1b',
+    },
+    fonts: {
+      body: 'Raleway, sans-serif',
+    },
+    fontWeights: {
+      body: 400,
+      heading: 700,
+      bold: 900,
+    },
+  
+    fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+  
+    space: [0, 2, 4, 8, 16, 32, 64, 128],
+  
+    borders: {
+      heading: '5px solid green',
+    },
+    radii: {
+      heading: '10px',
+    },
+    shadows: {
+      boxShadow: '4px 4px 33px 15px rgba(69, 75, 27, 0.8)',
+    },
+  };
+  
